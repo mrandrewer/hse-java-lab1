d8abe04908ed7f379abacd34894cf60094803824
Andrei Belskii <mrandrewer@yandex.ru>
2026-02-20 23:36:24 +0300

    Add alternative cycles

--- src/main/java/com/hse/lab1/App.java
+++ src/main/java/com/hse/lab1/App.java
@@ -205,8 +205,8 @@ public class App {
      * 
      * @param scanner Сканер для получения данных ввода
      */
-    public static void task3(Scanner scanner) {
-        System.out.println("Вычисление среднего значения последовательности");
+    public static void task3_for(Scanner scanner) {
+        System.out.println("Вычисление среднего значения последовательности через for");
         int n = readInt(scanner, "Введите число элементов последовательности > ");
         if (n < 1) {
             System.out.println("Количество элементов не может быть меньше 1");
@@ -221,12 +221,102 @@ public class App {
     }
 
     /**
+     * Задача 3. Вычисление среднего значения
+     * 
+     * @param scanner Сканер для получения данных ввода
+     */
+    public static void task3_while(Scanner scanner) {
+        System.out.println("Вычисление среднего значения последовательности через while");
+        int n = readInt(scanner, "Введите число элементов последовательности > ");
+        if (n < 1) {
+            System.out.println("Количество элементов не может быть меньше 1");
+            return;
+        }
+        double sum = 0;
+        int i = 0;
+        while (i < n) {
+            double elem = readInt(scanner, String.format("Введите элемент %d > ", i + 1));
+            sum += elem / n;
+            i++;
+        }
+        System.out.println(String.format("Среднее значение: %f", sum));
+    }
+
+    /**
+     * Задача 3. Вычисление среднего значения
+     * 
+     * @param scanner Сканер для получения данных ввода
+     */
+    public static void task3_dowhile(Scanner scanner) {
+        System.out.println("Вычисление среднего значения последовательности через do while");
+        int n = readInt(scanner, "Введите число элементов последовательности > ");
+        if (n < 1) {
+            System.out.println("Количество элементов не может быть меньше 1");
+            return;
+        }
+        double sum = 0;
+        int i = 0;
+        do {
+            double elem = readInt(scanner, String.format("Введите элемент %d > ", i + 1));
+            sum += elem / n;
+            i++;
+        } while (i < n);
+        System.out.println(String.format("Среднее значение: %f", sum));
+    }
+
+    /**
      * Задача 4. Вычисление минимального значения
      * 
      * @param scanner Сканер для получения данных ввода
      */
-    public static void task4(Scanner scanner) {
-        System.out.println("Вычисление минимального значения последовательности");
+    public static void task4_for(Scanner scanner) {
+        System.out.println("Вычисление минимального значения последовательности через for");
+        System.out.println("Введите 0 для завершения ввода значений");
+        int min = Integer.MAX_VALUE;
+        int cur = Integer.MAX_VALUE;
+        for (; cur != 0;) {
+            cur = readInt(scanner, "Введите элемент > ");
+            if (cur != 0 && cur < min) {
+                min = cur;
+            }
+        }
+        if (min == Integer.MAX_VALUE) {
+            System.out.println("Вы не ввели ни одного элемента!");
+        } else {
+            System.out.println(String.format("Минимальное значение: %d", min));
+        }
+    }
+
+    /**
+     * Задача 4. Вычисление минимального значения
+     * 
+     * @param scanner Сканер для получения данных ввода
+     */
+    public static void task4_while(Scanner scanner) {
+        System.out.println("Вычисление минимального значения последовательности через while");
+        System.out.println("Введите 0 для завершения ввода значений");
+        int min = Integer.MAX_VALUE;
+        int cur = Integer.MAX_VALUE;
+        while (cur != 0) {
+            cur = readInt(scanner, "Введите элемент > ");
+            if (cur != 0 && cur < min) {
+                min = cur;
+            }
+        }
+        if (min == Integer.MAX_VALUE) {
+            System.out.println("Вы не ввели ни одного элемента!");
+        } else {
+            System.out.println(String.format("Минимальное значение: %d", min));
+        }
+    }
+
+    /**
+     * Задача 4. Вычисление минимального значения
+     * 
+     * @param scanner Сканер для получения данных ввода
+     */
+    public static void task4_dowhile(Scanner scanner) {
+        System.out.println("Вычисление минимального значения последовательности через do while");
         System.out.println("Введите 0 для завершения ввода значений");
         int min = Integer.MAX_VALUE;
         int cur = 0;
@@ -248,17 +338,59 @@ public class App {
      * 
      * @param scanner Сканер для получения данных ввода
      */
-    public static void task5(Scanner scanner) {
-        System.out.println("Вычисление элемента последовательности P");
+    public static void task5_for(Scanner scanner) {
+        System.out.println("Вычисление элемента последовательности P через for");
         int n = readInt(scanner, "Введите номер элемента последовательности > ");
         if (n < 1) {
             System.out.println("Номер элемента не может быть меньше 1");
             return;
         }
-        double p = 2. / 3;
-        for (int i = 2; i <= n; i++) {
+        double p = 1;
+        for (int i = 1; i <= n; i++) {
             p *= (2. * i) / (2. * i + 1);
         }
+        System.out.println(String.format("Значение элемента %d: %f", n, p));
+    }
+
+    /**
+     * Задача 4. Вычисление минимального значения
+     * 
+     * @param scanner Сканер для получения данных ввода
+     */
+    public static void task5_while(Scanner scanner) {
+        System.out.println("Вычисление элемента последовательности P через while");
+        int n = readInt(scanner, "Введите номер элемента последовательности > ");
+        if (n < 1) {
+            System.out.println("Номер элемента не может быть меньше 1");
+            return;
+        }
+        double p = 1;
+        int i = 1;
+        while (i <= n) {
+            p *= (2. * i) / (2. * i + 1);
+            i++;
+        }
+        System.out.println(String.format("Значение элемента %d: %f", n, p));
+    }
+
+    /**
+     * Задача 4. Вычисление минимального значения
+     * 
+     * @param scanner Сканер для получения данных ввода
+     */
+    public static void task5_dowhile(Scanner scanner) {
+        System.out.println("Вычисление элемента последовательности P через do while");
+        int n = readInt(scanner, "Введите номер элемента последовательности > ");
+        if (n < 1) {
+            System.out.println("Номер элемента не может быть меньше 1");
+            return;
+        }
+        double p = 1;
+        int i = 1;
+        do {
+            p *= (2. * i) / (2. * i + 1);
+            i++;
+        } while (i <= n);
         System.out.println(String.format("Значение элемента %d: %f", n, p));
     }
 
@@ -271,9 +403,16 @@ public class App {
         System.out.println("0 - Выход");
         System.out.println("1 - Проверка приналежности точки фигуре");
         System.out.println("2 - Вычисление значения выражения");
-        System.out.println("3 - Вычисление среднего значения");
-        System.out.println("4 - Вычисление минимального значения");
-        System.out.println("5 - Вычисление элемента ряда");
+        System.out.println("3 - Вычисление среднего значения через for");
+        System.out.println("4 - Вычисление среднего значения через while");
+        System.out.println("5 - Вычисление среднего значения через do while");
+        System.out.println("6 - Вычисление минимального значения через for");
+        System.out.println("7 - Вычисление минимального значения через while");
+        System.out.println("8 - Вычисление минимального значения через do while");
+        System.out.println("9 - Вычисление элемента ряда через for");
+        System.out.println("10 - Вычисление элемента ряда через while");
+        System.out.println("11 - Вычисление элемента ряда через do while");
+        System.out.print("Введите номер пункта > ");
     }
 
     /**
@@ -300,13 +439,31 @@ public class App {
                         task2();
                         break;
                     case 3:
-                        task3(scanner);
+                        task3_for(scanner);
                         break;
                     case 4:
-                        task4(scanner);
+                        task3_while(scanner);
                         break;
                     case 5:
-                        task5(scanner);
+                        task3_dowhile(scanner);
+                        break;
+                    case 6:
+                        task4_for(scanner);
+                        break;
+                    case 7:
+                        task4_while(scanner);
+                        break;
+                    case 8:
+                        task4_dowhile(scanner);
+                        break;
+                    case 9:
+                        task5_for(scanner);
+                        break;
+                    case 10:
+                        task5_while(scanner);
+                        break;
+                    case 11:
+                        task5_dowhile(scanner);
                         break;
                     case 0:
                         System.out.println("Выход из программы...");
@@ -316,7 +473,7 @@ public class App {
                         System.out.println("Неверный выбор. Попробуйте снова.");
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Ошибка! Некорректный вводв.");
+                System.out.println("Ошибка! Некорректный ввод.");
                 scanner.nextLine(); // Очистка буфера
             }
         }
